38dc206cc091a794e251298b96adb74c1db83baa
siab01 <abdallah.silima1@gmail.com>
2023-09-19 11:06:35 +0200

    Phase 8 : ORM (affichage en colonne des informations provenant de la BDD).

--- src/Controller/VoyagesConrtoller.php
+++ src/Controller/VoyagesConrtoller.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VisitesRepository;
 
 /**
  * Description of VoyagesConrtoller
@@ -13,11 +14,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class VoyagesConrtoller extends AbstractController{
 
     /**
+     * 
+     * @var VisitesRepository
+     */
+    private $reposority ;
+
+    public function
+    __construct(VisitesRepository $reposority )
+    {
+        $this->reposority = $reposority ;
+        
+    }
+
+    /**
      * @Route("/voyages", name="voyages")
      * @return Response
      */
     public function index(): Response{
-        return $this->render("pages/voyages.html.twig");
+       $lesvisites =  $this->reposority->findAll();
+       
+        return $this->render("pages/voyages.html.twig", ['visites' => $lesvisites]);
     }
 
 }
